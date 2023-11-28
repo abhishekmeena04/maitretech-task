34380ad229bdc23481a9967b37d83058ad4c4bc2
Abhishek Meena <abhishekmeena7452@gmail.com>
2023-11-28 02:45:13 -0800

    page5

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Page2 from "./components/Page2";
 import Page3 from "./components/Page3";
 import Page4 from "./components/Page4";
+import Page5 from "./components/Page5";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Page2 />
       <Page3 />
       <Page4 />
+      <Page5 />
     </>
   );
 };
---
+++ src/components/Page5.jsx
@@ -0,0 +1,127 @@
+import React from "react";
+
+const Page5 = () => {
+  return (
+    <>
+      <div className="page5">
+        <div className="our">Student Work</div>
+        <div className="cards">
+          <div className="card">
+            <img
+              src="https://www.nifafinearts.com/images/homegallery/img1.jpg"
+              alt=""
+            />
+          </div>
+          <div className="card">
+            <img
+              src="https://www.nifafinearts.com/images/homegallery/img2.jpg"
+              alt=""
+            />
+          </div>
+          <div className="card">
+            <img
+              src="https://www.nifafinearts.com/images/homegallery/img3.jpg"
+              alt=""
+            />
+          </div>
+          <div className="card">
+            <img
+              src="https://www.nifafinearts.com/images/homegallery/img4.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="cards">
+          <div className="cart">
+            <img
+              src="https://www.nifafinearts.com/images/homegallery/img5.JPG"
+              alt=""
+            />
+          </div>
+          <div className="cart">
+            <img
+              src="https://www.nifafinearts.com/images/homegallery/img6.JPG"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <img
+              src="https://www.nifafinearts.com/images/homegallery/img7.jpg"
+              alt=""
+            />
+          </div>
+          <div className="card">
+            <img
+              src="https://www.nifafinearts.com/images/homegallery/img8.jpg"
+              alt=""
+            />
+          </div>
+          <div className="card">
+            <img
+              src="https://www.nifafinearts.com/images/homegallery/img12.jpg"
+              alt=""
+            />
+          </div>
+          <div className="card">
+            <img
+              src="https://www.nifafinearts.com/images/homegallery/img11.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="cards">
+          <div className="cart">
+            <img
+              src="https://www.nifafinearts.com/images/homegallery/img13.jpg"
+              alt=""
+            />
+          </div>
+          <div className="cart">
+            <img
+              src="https://www.nifafinearts.com/images/homegallery/img14.jpeg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <img
+              src="https://www.nifafinearts.com/images/homegallery/img18.jpeg"
+              alt=""
+            />
+          </div>
+          <div className="card">
+            <img
+              src="https://www.nifafinearts.com/images/homegallery/img19.jpg"
+              alt=""
+            />
+          </div>
+          <div className="card">
+            <img
+              src="https://www.nifafinearts.com/images/homegallery/img20.jpeg"
+              alt=""
+            />
+          </div>
+          <div className="card">
+            <img
+              src="https://www.nifafinearts.com/images/homegallery/img21.jpeg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="cards">
+          <div className="car">
+            <img
+              src="https://www.nifafinearts.com/images/homegallery/img22.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Page5;
